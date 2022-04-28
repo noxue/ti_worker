c3d3d61985a0922c10a0ccc0fd0fceb8e51625e9
noxue <173126019@qq.com>
2022-04-29 00:20:21 +0800

    正常版本

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,45 @@
 use rand::Rng;
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+    thread,
+    time::Duration,
+};
+use ti_protocol::{get_header_size, PackType, Packet, Task, TaskResult, TiPack, TiUnPack};
 
-use ti_protocol::{PackType, Packet, Task, TaskResult, TiPack};
-use tokio::{io::AsyncWriteExt, net::TcpStream};
-
-#[tokio::main]
-async fn main() {
+fn main() {
     // 连接服务端
-    let mut stream = TcpStream::connect("192.168.1.6:8000").await.unwrap();
-    // loop {
-    let mut rng = rand::thread_rng();
-    let x: i32 = rng.gen_range(10..1000);
-    let y: i32 = rng.gen_range(10..1000);
+    let mut stream = TcpStream::connect("192.168.1.6:8000").unwrap();
+    loop {
+        let mut rng = rand::thread_rng();
+        let x: i32 = rng.gen_range(10..1000);
+        let y: i32 = rng.gen_range(10..1000);
 
-    // 创建Task数据包
-    let task = Task::new(x, format!("{}+{}", x, y));
-    // 创建Packet
-    let packet = Packet::new(PackType::Task, task).unwrap();
+        // 创建Task数据包
+        let task = Task::new(x, format!("{}+{}", x, y));
+        // 创建Packet
+        let packet = Packet::new(PackType::Task, task).unwrap();
 
-    // 将packet序列化
-    let packet = packet.pack().unwrap();
+        // 将packet序列化
+        let packet = packet.pack().unwrap();
+        println!("packet: {:?}", packet);
 
-    // 发送
-    stream.write(&packet).await.unwrap();
+        let t = Task::unpack(&packet[get_header_size()..]).unwrap();
+        println!("{:?}", t);
 
-    // 创建 TaskResult
-    let task_result = TaskResult::new(x, Ok(x + y));
-    // 创建Packet
-    let packet = Packet::new(PackType::TaskResult, task_result).unwrap();
-    // 将packet序列化
-    let packet = packet.pack().unwrap();
-    // 发送
-    stream.write(&packet).await.unwrap();
-    stream.flush().await.unwrap();
-    // }
+        // 发送
+        stream.write(&packet).unwrap();
+        stream.flush().unwrap();
+        // 创建 TaskResult
+        let task_result = TaskResult::new(x, Ok(x + y));
+        // 创建Packet
+        let packet = Packet::new(PackType::TaskResult, task_result).unwrap();
+        // 将packet序列化
+        let packet = packet.pack().unwrap();
+        // 发送
+        stream.write(&packet).unwrap();
+        stream.flush().unwrap();
+    }
 
     // // 发送数据
     // stream.write(b"hello world!").unwrap();
